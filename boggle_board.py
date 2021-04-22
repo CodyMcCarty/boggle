ef424f3750f9cbd83a58ed806ce862701da0356d
Cody McCarty <codyjmccarty@gmail.com>
2021-04-22 10:31:30 -0500

    initial commit

--- boggle_board.py
+++ boggle_board.py
@@ -1,6 +1,10 @@
 class BoggleBoard:
   
   def __init__(self):
+    # building out the necessary data structure to emulate our board
+    
   
 
   def shake(self):
+    pass
+    # should modify the board by filling each cell with a random upper-case letter
